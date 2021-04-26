f3c20c088caa2994804a55f6508a5a3dbe7a3a56
svarkhinos <svitlana.arkhinos@gmail.com>
2021-04-26 23:31:02 +0300

    Update task 06

--- task-06.js
+++ task-06.js
@@ -4,17 +4,37 @@ const DATA_LENGTH = Number(input.getAttribute('data-length'));
 
 
 input.addEventListener('blur', onInputBlur);
+input.addEventListener('focus', onInputFocus);
 
 
 function onInputBlur(event) {
+     
+  return event.currentTarget.value.length === DATA_LENGTH ?
+    input.classList.add('valid') :
     input.classList.add('invalid');
-    input.classList.remove('valid');
-    
-    const inputLength = event.currentTarget.value.length;
+} 
 
-    if (inputLength === DATA_LENGTH) {
-        input.classList.add('valid');
-        input.classList.remove('invalid');
-    } 
-
+function onInputFocus() {
+input.removeAttribute('class')
 }
+
+
+
+
+
+// function onInputBlur(event) {
+//     input.classList.add('invalid');
+//     input.classList.remove('valid');
+    
+//     const inputLength = event.currentTarget.value.length;
+
+//     if (inputLength === DATA_LENGTH) {
+//         input.classList.add('valid');
+//         input.classList.remove('invalid');
+//     } 
+
+// }
+
+
+
+
